f84e02d747fbfe46c1797821107a8456d2ff5dea
WEBabuzer228 <86408458+WEBabuzer228@users.noreply.github.com>
2022-05-31 01:29:49 +0300

    Add files via upload

---
+++ parserSite/index.php
@@ -0,0 +1,35 @@
+
+<html lang="ru">
+<head>
+    <meta charset="UTF-8"
+    <title>  </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header id="header" class="header">
+    <div class="container">
+        <div class="nav">
+            <img class="logo" src="png/GoodsLogo.png" alt="GoodsCHECK">
+            <li>
+                <a href="http://goodscheck.local/register.php">
+                    Регистрация
+                </a>
+            </li>
+
+            <li>
+                </a>
+                <a href="http://goodscheck.local/login.php">
+                    Войти
+                </a>
+            </li>
+
+        </div>
+
+    </div>
+</header>
+
+</body>
+</html>
